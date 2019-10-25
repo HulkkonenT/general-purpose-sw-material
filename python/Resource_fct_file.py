846698ca2ae53ef31026bfa544523484d6e52682
Timo Hulkkonen <timo.hulkkonen1@gmail.com>
2019-10-25 11:53:34 +0300

    Resource_fct_file.py
    Resource file for demonstrating functions.

---
+++ python/Resource_fct_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/ python
+
+def fct2():
+    print("resun fct2")
+
+def fct3():
+    print("Age {0} questionnaire:".format("Person"))
+    age = input("Age, please: ")
+    print(age)
+    print("Age {0} multiplied by {1}".format(age,5),age*5)
